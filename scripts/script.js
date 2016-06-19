2724ffbd90d6679a9ff96f811295d37c59b7f602
Bill Bouzas <bill_bouzas@hotmail.com>
2016-06-19 15:11:26 +0300

    init commit

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+var checkboxes = document.querySelectorAll("input[name=\"category\"]");
+var movies = document.getElementById("movies");
+var moviesInDiv = movies.childNodes;
+
+
+
+var anyCheckbox = checkboxes[0];
+
+// anyCheckbox.addEventListener("click", function() {
+//     if (anyCheckbox.checked) {
+//         checkboxes.forEach(function(check, i, checkboxes)
+//         { check.setAttribute("checked", ""); });
+//     }
+//     else {
+//         checkboxes.forEach(function(check, i, checkboxes)
+//         { check.removeAttribute("checked"); });
+//     }
+// });
+
+anyCheckbox.addEventListener("click", createMovie);
+
+function createMovie() {
+    var movieDiv = document.createElement("div");
+    movieDiv.className = "movie";
+
+    var title = document.createElement("p");
+    var movieThumb = document.createElement("img");
+    var rating = document.createElement("p");
+
+    title.innerHTML = "Mission Impossible";
+
+    movieThumb.setAttribute("src", "images/test.jpg");
+    movieThumb.className = "movieThumb";
+
+    rating.innerHTML = "Βαθμολογία: 4.2/5";
+
+    movieDiv.appendChild(title);
+    movieDiv.appendChild(movieThumb);
+    movieDiv.appendChild(rating);
+
+    movies.appendChild(movieDiv);
+
+    moviesInDiv[moviesInDiv.length-1].addEventListener("click", showMovieInfo);
+}
+
+function showMovieInfo() {
+    location = "movieInfo.html";
+}
